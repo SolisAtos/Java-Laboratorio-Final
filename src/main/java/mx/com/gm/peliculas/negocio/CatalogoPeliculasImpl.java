09d09a4534d513274791e553f6dd95614e764c4c
Jose Antonio Solis Martinez <jose-antonio.solis@atos.net>
2022-09-05 10:56:53 -0500

    clases del paquete de negocio

--- src/main/java/mx/com/gm/peliculas/negocio/CatalogoPeliculasImpl.java
+++ src/main/java/mx/com/gm/peliculas/negocio/CatalogoPeliculasImpl.java
@@ -1,5 +1,36 @@
 package mx.com.gm.peliculas.negocio;
 
-public class CatalogoPeliculasImpl {
+import mx.com.gm.peliculas.datos.AccesoDatos;
+
+public class CatalogoPeliculasImpl implements CatalogoPeliculas {
+    AccesoDatos datos;
+
+    public CatalogoPeliculasImpl() {
+
+    }
+
+    @Override
+    public void agregarPelicula(String nombrePelicula, String nombreArchivo) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void listarPeliculas(String nombreArchivo) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void buscarPelicula(String nombreArchivo, String buscar) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void iniciarArchivo(String nombreArchivo) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
