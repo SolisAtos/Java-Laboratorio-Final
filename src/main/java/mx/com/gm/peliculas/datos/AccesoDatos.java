859327acf591e34e2fd24384535b0e9612f78396
Jose Antonio Solis Martinez <jose-antonio.solis@atos.net>
2022-09-05 10:53:16 -0500

    Interfaz acceso datos

--- src/main/java/mx/com/gm/peliculas/datos/AccesoDatos.java
+++ src/main/java/mx/com/gm/peliculas/datos/AccesoDatos.java
@@ -1,5 +1,19 @@
 package mx.com.gm.peliculas.datos;
 
-public interface AccesoDatos {
+import java.util.List;
 
+import mx.com.gm.peliculas.domain.Pelicula;
+
+public interface AccesoDatos {
+    public boolean existe(String nombreArchivo);
+
+    public List<Pelicula> listar(String nombre);
+
+    public void escribir(Pelicula pelicula, String nombreArchivo, Boolean anexar);
+
+    public String buscar(String nombreArchivo, String buscar);
+
+    public void crear(String nombreArchivo);
+
+    public void borrar(String nombreArchivo);
 }
