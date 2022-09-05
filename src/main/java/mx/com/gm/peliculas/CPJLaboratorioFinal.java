a50a8f2a51296b8def26a991a61cfe46291f0a7b
Jose Antonio Solis Martinez <jose-antonio.solis@atos.net>
2022-09-05 10:47:10 -0500

    first commit

---
+++ src/main/java/mx/com/gm/peliculas/CPJLaboratorioFinal.java
@@ -0,0 +1,18 @@
+package mx.com.gm.peliculas;
+
+/**
+ * Hello world!
+ */
+public final class CPJLaboratorioFinal {
+    private CPJLaboratorioFinal() {
+    }
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
